06f1a70cbadce3f8aea94ea7099ef6c2f717e69a
RistoLibera <qofgxnm7@gmail.com>
2021-04-22 10:22:29 +0900

    Update searching

--- app/controllers/searchings_controller.rb
+++ app/controllers/searchings_controller.rb
@@ -3,33 +3,37 @@ class SearchingsController < ApplicationController
   def create
     @word = searching_params[:word]
     @searching = current_user.searchings.build(word: @word)
-    flash[:notice] = "#{@searching.valid?}" 
-    @users_email = User.where("email LIKE ?", "%#{@word}%")
+    flash[:notice] = "New searching is performed!" if @searching.save
+    @users = User.where("firstname LIKE ? OR lastname LIKE ?", "%#{@word}%", "%#{@word}%")
     @posts = Post.where("body LIKE ?", "%#{@word}%")
     render :index
   end
 
   def index
-    @users_email = User.where("email LIKE ?", @search_word)
-    @posts = Post.where("body LIKE ?", "%#{@search_word}%")
+    if current_user.searchings.any?
+      @word = current_user.searchings.last.word
+      @users = User.where("firstname LIKE ? OR lastname LIKE ?", "%#{@word}%", "%#{@word}%")
+      @posts = Post.where("body LIKE ?", "%#{@word}%")
+    else
+      @users = nil
+      @posts = nil
+    end
   end 
 
   def destroy
-    @search = current_user.searches.find(params[:id])
-    if search.destroy
+    @search = current_user.searchings.find(params[:id])
+    if @search.destroy
       flash[:notice] = "Searching is deleted!"
-      redirect_to searches_path
+      redirect_back fallback_location: posts_path
     else
       render 'index'
     end
 
   end
 
-  # Book.where("title LIKE ?", "%" + params[:q] + "%")
   private
 
   def searching_params
     params.require(:searching).permit(:word)
   end
-
 end
